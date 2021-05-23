45d06535a20700812b5752a064e841571e487301
mattaiod <matteodelandhuypro@outlook.fr>
2021-05-23 15:48:08 +0200

    app clean + stored

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,43 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+/* Example STORE
+source: [
+  {
+    inputFormat,
+    file: '',
+    nameCard: '',
+    teamLabel: '',
+    triggerType: '',
+    dateUpdateExpected: '',
+  }
+]*/
+
 export default new Vuex.Store({
   state: {
+    sources: [
+      {
+        inputFormat: 'Fichier',
+        file: 'txt.txt',
+        nameCard: 'Carte de production des poires',
+        teamLabel: 'production',
+        triggerType: 'Manuel',
+        dateUpdateExpected: '2020-05-07',
+      }
+    ]
+  },
+  getters: {
+    sources: (state) => state.sources,
   },
   mutations: {
+    SET_NEW_SOURCE(state, source) {
+      state.sources.push(source)
+    },
   },
   actions: {
+    setNewSource({ commit }, payload) {
+      commit('SET_NEW_SOURCE', payload)
+    },
   },
   modules: {
   }
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Sources msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Sources from '@/components/Sources.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Sources
+  }
+}
+</script>
